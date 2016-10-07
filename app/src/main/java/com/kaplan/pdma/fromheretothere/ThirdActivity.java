b144253d6301b9fddbed1bbdbaf9d6dd981eed9f
William Ku <kucheech@gmail.com>
2016-10-07 19:56:25 +0800

    added code for ThirdActivity

--- app/src/main/java/com/kaplan/pdma/fromheretothere/ThirdActivity.java
+++ app/src/main/java/com/kaplan/pdma/fromheretothere/ThirdActivity.java
@@ -1,7 +1,10 @@
 package com.kaplan.pdma.fromheretothere;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        ImageView car3 = (ImageView) findViewById(R.id.imageViewCar3);
+        car3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ThirdActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        car3.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                finish(); //go back to first screen
+                return true;
+            }
+        });
     }
 }
